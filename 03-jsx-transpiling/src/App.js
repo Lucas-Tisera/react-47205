93a444155f05f493be9f4177ba61d58013f749b9
Lucas <lucastisera02@gmail.com>
2023-09-12 20:56:37 -0300

    Feat clase Componentes II

--- 03-jsx-transpiling/src/App.js
+++ 03-jsx-transpiling/src/App.js
@@ -41,21 +41,63 @@ import './App.css';
   ); */
 import FormularioLogin from "./components/Formularios/formularioLogin"
 import FormularioRegistro from "./components/Formularios/formularioRegistro"
-import { useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 function App() {
   const [numero3, setNumero3] = useState(3)
   console.log(numero3)
   const [tipoFormulario , setTipoFormulario] = useState("login")
   const [themeRed, setThemeRed] = useState(true)
+
+  const FORM2 =memo(({children})=> {
+    return (
+      <div>
+          {children}
+        <h1>
+          Los de arriba son mis elementos hijos
+        </h1>
+      </div>
+    )
+  })
+  
+  const Titulo =({titulo})=> {
+    return (
+      <div>
+        <h1>
+          {titulo}
+        </h1>
+      </div>
+    )
+  }
+  
+  useEffect(()=>{
+    console.log("Se ejecuto el useEffect")
+  },[tipoFormulario])
+
+
   return (
     <div>
+      <h1>{tipoFormulario}</h1>
       <h1 className={themeRed ? "miTituloRojo" : "miTituloAzul"}>React App</h1>
-      {
+      <input type='button' onClick={() =>setThemeRed(!themeRed) } value={"Cambiar Titulo"}/>
+      <input type="button" onClick={() => setTipoFormulario("Registro")} value={"Registrarme"}/>
+
+      {/* <Titulo titulo={"Titulo del formulario"}>
+
+      </Titulo>
+
+      <FORM2>
+        <h1>
+          Chau
+        </h1>
+        <input type="button"></input>
+      </FORM2> */}
+
+      {/* {
        (tipoFormulario === "login") ? 
        <FormularioLogin themeRed={themeRed} setThemeRed={setThemeRed} numeros={4} titulo="MI TITULO"/> : 
        <FormularioRegistro/>
-      }
-      <button onClick={() => setTipoFormulario("registro")}>Registrarme</button>
+      } */}
+      {/* <button onClick={() => setTipoFormulario("registro")}>Registrarme</button> */}
     </div>
   )
 }
